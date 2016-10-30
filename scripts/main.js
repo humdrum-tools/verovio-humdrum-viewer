d118060e1ca3c4dc756a9057a059e5ab2483d74a
Craig Sapp <craigsapp@gmail.com>
2016-10-30 00:14:48 -0700

    Added fix for chords.

--- scripts/main.js
+++ scripts/main.js
@@ -30,7 +30,7 @@ var CursorNote;
 
 // Increment BasketVersion when the verovio toolkit is updated, or
 // the Midi player software or soundfont is updated.
-var BasketVersion = 70;
+var BasketVersion = 71;
 
 var Actiontime = 0;
 
