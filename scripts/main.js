37565b592d9d0c31094c0ab3a1c86afb0c9135a0
Craig Sapp <craigsapp@gmail.com>
2016-11-19 22:10:15 -0800

    added key-commands.txt.

--- scripts/main.js
+++ scripts/main.js
@@ -2387,7 +2387,7 @@ function convertTokenToCsv(token) {
 
 function prepareHelpMenu(selector) {
 	var request = new XMLHttpRequest();
-	request.open("GET", "/key-commands.html");
+	request.open("GET", "/scripts/key-commands.txt");
 	request.addEventListener("load", function() {
 		fillInHelpContainer(selector, request.responseText);
 	});
