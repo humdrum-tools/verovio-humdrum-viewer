13af0239da86012156d1a487c41919264e343417
Craig Sapp <craigsapp@gmail.com>
2016-12-09 22:24:18 -0800

    Updated for **silbe

--- scripts/main.js
+++ scripts/main.js
@@ -34,7 +34,7 @@ var RestoreCursorNote;
 
 // Increment BasketVersion when the verovio toolkit is updated, or
 // the Midi player software or soundfont is updated.
-var BasketVersion = 104;
+var BasketVersion = 105;
 
 var Actiontime = 0;
 
