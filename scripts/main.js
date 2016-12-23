54b927f8a1294f22f3b5e0755c91774266686c63
Craig Sapp <craigsapp@gmail.com>
2016-12-23 07:40:43 -0800

    Updated for double barlines.

--- scripts/main.js
+++ scripts/main.js
@@ -34,7 +34,7 @@ var RestoreCursorNote;
 
 // Increment BasketVersion when the verovio toolkit is updated, or
 // the Midi player software or soundfont is updated.
-var BasketVersion = 118;
+var BasketVersion = 119;
 
 var Actiontime = 0;
 
