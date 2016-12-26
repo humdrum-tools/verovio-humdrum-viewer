a8f7b7ae39f52969a799bef94d0ce146a55cc085
Craig Sapp <craigsapp@gmail.com>
2016-12-25 21:59:16 -0800

    Fix some memory leaks.

--- scripts/main.js
+++ scripts/main.js
@@ -34,7 +34,7 @@ var RestoreCursorNote;
 
 // Increment BasketVersion when the verovio toolkit is updated, or
 // the Midi player software or soundfont is updated.
-var BasketVersion = 133;
+var BasketVersion = 134;
 
 var Actiontime = 0;
 
