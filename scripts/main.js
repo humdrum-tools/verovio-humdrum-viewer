d163fd74e50934e6a74b2c1896f862f4973671ee
Craig Sapp <craigsapp@gmail.com>
2017-02-06 22:58:22 -0800

    Trill accidentals.

--- scripts/main.js
+++ scripts/main.js
@@ -39,7 +39,7 @@ var RestoreCursorNote;
 
 // Increment BasketVersion when the verovio toolkit is updated, or
 // the Midi player software or soundfont is updated.
-var BasketVersion = 188;
+var BasketVersion = 189;
 
 var Actiontime = 0;
 
