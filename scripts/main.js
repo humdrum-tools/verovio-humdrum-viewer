3e025a0915615022222f4011cb1e2624a001c3d9
Craig Sapp <craigsapp@gmail.com>
2017-05-04 22:34:57 -0700

    Added editorial accidental conversion from MusicXML.

--- scripts/main.js
+++ scripts/main.js
@@ -39,7 +39,7 @@ var RestoreCursorNote;
 
 // Increment BasketVersion when the verovio toolkit is updated, or
 // the Midi player software or soundfont is updated.
-var BasketVersion = 211;
+var BasketVersion = 212;
 
 var Actiontime = 0;
 
