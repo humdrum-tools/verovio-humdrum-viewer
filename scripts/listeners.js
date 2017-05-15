f91f986118a08c27398568008bcc87b82441d55a
Craig Sapp <craigsapp@gmail.com>
2017-05-15 12:42:52 +0200

    Added local running of website.

--- scripts/listeners.js
+++ scripts/listeners.js
@@ -1,3 +1,6 @@
+---
+permalink: /scripts/listeners.js
+---
 //
 // Programmer:     Craig Stuart Sapp <craig@ccrma.stanford.edu>
 // Creation Date:  Sun Apr 17 17:21:46 PDT 2016
@@ -16,7 +19,11 @@
 //
 
 document.addEventListener("DOMContentLoaded", function() {
-	downloadVerovioToolkit('http://verovio-script.humdrum.org/scripts/verovio-toolkit.js');
+	{% if site.local == "yes" %}
+		downloadVerovioToolkit('/scripts/local/verovio-toolkit.js');
+	{% else %}
+		downloadVerovioToolkit('http://verovio-script.humdrum.org/scripts/verovio-toolkit.js');
+	{% endif %}
 
 	CGI = GetCgiParameters();
 
@@ -64,12 +71,14 @@ document.addEventListener("DOMContentLoaded", function() {
 	$("#input").data('x', $("#input").outerWidth());
 	$("#input").data('y', $("#input").outerHeight());
 
+{% if site.local != "yes" %}
   (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
   (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
   m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
   })(window,document,'script','https://www.google-analytics.com/analytics.js','ga');
   ga('create', 'UA-82554203-1', 'auto');
   ga('send', 'pageview');
+{% endif %}
 
 	var body = document.querySelector("body");
 	body.addEventListener("click", function(event) {
