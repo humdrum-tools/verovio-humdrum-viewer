76e3947eb8912bef7eb5141ce3ea8e8a57b36909
Craig Sapp <craigsapp@gmail.com>
2017-06-01 22:39:46 +0200

    Updated cint tool.

--- scripts/main.js
+++ scripts/main.js
@@ -44,7 +44,7 @@ var RestoreCursorNote;
 
 // Increment BasketVersion when the verovio toolkit is updated, or
 // the Midi player software or soundfont is updated.
-var BasketVersion = 232;
+var BasketVersion = 233;
 
 var Actiontime = 0;
 
