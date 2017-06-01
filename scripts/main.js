bf6f52b60373b896fa08db07ce1b898c5fa564c1
Craig Sapp <craigsapp@gmail.com>
2017-06-01 14:15:31 +0200

    Updated for dissonant -s

--- scripts/main.js
+++ scripts/main.js
@@ -44,7 +44,7 @@ var RestoreCursorNote;
 
 // Increment BasketVersion when the verovio toolkit is updated, or
 // the Midi player software or soundfont is updated.
-var BasketVersion = 231;
+var BasketVersion = 232;
 
 var Actiontime = 0;
 
