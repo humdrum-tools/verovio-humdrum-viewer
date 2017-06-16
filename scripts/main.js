055226b51fc91cfda4a9727af2e9e99f6354853c
Craig Sapp <craigsapp@gmail.com>
2017-06-17 00:56:16 +0200

    Update

--- scripts/main.js
+++ scripts/main.js
@@ -44,7 +44,7 @@ var RestoreCursorNote;
 
 // Increment BasketVersion when the verovio toolkit is updated, or
 // the Midi player software or soundfont is updated.
-var BasketVersion = 254;
+var BasketVersion = 255;
 
 var Actiontime = 0;
 
