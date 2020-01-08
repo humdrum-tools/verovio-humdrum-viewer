6ac11931a6dea3e64e19dfcfb03040f1a4118371
Craig Stuart Sapp <craigsapp@gmail.com>
2020-01-08 04:43:29 -0800

    Uncomment.

--- scripts/listeners.js
+++ scripts/listeners.js
@@ -109,14 +109,12 @@ console.log("CGI", CGI);
 	$("#input").data('y', $("#input").outerHeight());
 
 {% if site.local != "true" %}
-/*
   (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
   (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
   m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
   })(window,document,'script','https://www.google-analytics.com/analytics.js','ga');
   ga('create', 'UA-82554203-1', 'auto');
   ga('send', 'pageview');
-*/
 {% endif %}
 
 	var body = document.querySelector("body");
