85233b5555ec5a9f44d80ec8b01111ee03bc6d45
Craig Stuart Sapp <craigsapp@gmail.com>
2020-08-25 20:17:59 -0700

    Implementation for issue https://github.com/humdrum-tools/verovio-humdrum-viewer/issues/382

--- scripts/main.js
+++ scripts/main.js
@@ -5230,3 +5230,68 @@ function getScaleFromPercentSize(string, baseScale) {
 }
 
 
+
+//////////////////////////////
+//
+// saveBuffer -- save the text contents to a local buffer, but if the
+//    shift key is pressed, delete the current contents of the buffer instead.
+//
+
+function saveBuffer(number, event) {
+	if (number < 1 || number > 9) {
+		return;
+	}
+	if (event && event.shiftKey) {
+		// clear contents of given buffer.
+		var key = "SAVE" + number;
+		delete localStorage[key];
+		var title = key + "-TITLE";
+		delete localStorage[title];
+		var selement = document.querySelector("#save-" + number);
+		if (selement) {
+			selement.classList.remove("filled");
+		}
+		var lelement = document.querySelector("#load-" + number);
+		if (lelement) {
+			lelement.classList.remove("filled");
+		}
+	} else {
+		// store contents of text editor in given buffer.
+		MENU.saveToBuffer(number);
+	}
+}
+
+
+
+//////////////////////////////
+//
+// loadBuffer -- load the text contents from a local buffer, but if the
+//    shift key is pressed, delete the current contents of the buffer instead.
+//
+
+function loadBuffer(number, event) {
+	if (number < 1 || number > 9) {
+		return;
+	}
+	if (event && event.shiftKey) {
+		// clear contents of given buffer.
+		var key = "SAVE" + number;
+		delete localStorage[key];
+		var title = key + "-TITLE";
+		delete localStorage[title];
+		var selement = document.querySelector("#save-" + number);
+		if (selement) {
+			selement.classList.remove("filled");
+		}
+		var lelement = document.querySelector("#load-" + number);
+		if (lelement) {
+			lelement.classList.remove("filled");
+		}
+	} else {
+		// store contents of text editor in given buffer.
+		MENU.loadFromBuffer(number);
+	}
+}
+
+
+
