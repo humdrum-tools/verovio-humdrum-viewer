9722b7d4413e18bb748262c6adf0f849f897d782
Craig Stuart Sapp <craigsapp@gmail.com>
2020-09-03 01:38:36 -0700

    Add link to toolbar documention on main toolbar.

--- scripts/main.js
+++ scripts/main.js
@@ -5604,6 +5604,17 @@ function showSearchHelp() {
 
 
 
+//////////////////////////////
+//
+// showToolbarHelp --
+//
+
+function showToolbarHelp() {
+	var help = window.open("https://doc.verovio.humdrum.org/interface/toolbar", "documentation");
+	help.focus();
+}
+
+
 
 //////////////////////////////
 //
