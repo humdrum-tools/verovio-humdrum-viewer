bea28e7e15c0988d41d4097ddd92118425ef0f98
Craig Stuart Sapp <craigsapp@gmail.com>
2023-02-28 10:32:44 -0800

    Add missing new file.

---
+++ _includes/vhv-scripts/filtering/detachGlobalVerovioOptions.js
@@ -0,0 +1,14 @@
+
+
+
+//////////////////////////////
+//
+// detachGlobalVerovioOptions --
+//
+
+function detachGlobalVerovioOptions() {
+	if (GLOBAL_VEROVIO_OPTIONS) {
+		GLOBAL_VEROVIO_OPTIONS = "";
+	}
+}
+
