a6a0108411de2cbce2e428ff6a168acbfd2633ed
Craig Stuart Sapp <craigsapp@gmail.com>
2024-03-08 09:58:11 -0800

    Add other SMuFL fonts for PDF creation.

--- _includes/vhv-scripts/pdf/loadPdfFonts.js
+++ _includes/vhv-scripts/pdf/loadPdfFonts.js
@@ -7,12 +7,21 @@
 
 function loadPdfFonts(pdf) {
 	return RSVP.all([
+		// Text font used on VHV:
 		loadFontResource(pdf, 'Times', '/scripts/pdfkit/EBGaramond-Regular.ttf'),
 		loadFontResource(pdf, 'TimesItalic', '/scripts/pdfkit/EBGaramond-Italic.ttf'),
 		loadFontResource(pdf, 'TimesBold', '/scripts/pdfkit/EBGaramond-Bold.ttf'),
 		loadFontResource(pdf, 'TimesBoldItalic', '/scripts/pdfkit/EBGaramond-BoldItalic.ttf'),
+
+		// no longer used:
 		loadFontResource(pdf, 'VerovioText', '/scripts/pdfkit/VerovioText-1.0.ttf'),
+
+		// SMuFL fonts that are available in VHV:
 		loadFontResource(pdf, 'Leipzig', '/scripts/pdfkit/Leipzig.ttf'),
+		loadFontResource(pdf, 'Leland', '/scripts/pdfkit/Leland.ttf'),
+		loadFontResource(pdf, 'Bravura', '/scripts/pdfkit/Bravura.ttf'),
+		loadFontResource(pdf, 'Gootville', '/scripts/pdfkit/GootVille.ttf'),
+		loadFontResource(pdf, 'Petaluma', '/scripts/pdfkit/Petaluma.ttf'),
 	]);
 }
 
