853a6d8fc30760e86f3af638c1e6a31cfe326eea
Craig Stuart Sapp <craigsapp@gmail.com>
2024-03-26 21:15:12 -0700

    Updates.

--- _includes/vhv-scripts/menu/makeYoutubeIcon.js
+++ _includes/vhv-scripts/menu/makeYoutubeIcon.js
@@ -10,7 +10,7 @@ function makeYoutubeIcon(url, title) {
 	title = title.replace(/"/g, "'");
 	var output = "<div title=\"" + title + "\" ";
 	output += "style='margin-left:10px !important; margin-right:0px !important; font-size:100%' ";
-	output += "onclick=\"window.open('" + url + "', '_blank')\" ";
+	output += "onclick=\"window.open('" + url + "', '_youtube')\" ";
 	output += "class='nav-icon fab fa-youtube'></div>";
 	return output;
 }
