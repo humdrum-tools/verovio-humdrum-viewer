09b90359bb8836ab70337a74b3cfb51d82edbf7d
Craig Stuart Sapp <craigsapp@gmail.com>
2025-03-18 02:57:04 -0700

    Update analytics code

--- _includes/vhv-scripts/listeners/DOMContentLoaded.js
+++ _includes/vhv-scripts/listeners/DOMContentLoaded.js
@@ -224,12 +224,13 @@ document.addEventListener("DOMContentLoaded", function() {
 	$("#input").data('y', $("#input").outerHeight());
 
 {% if site.local != "true" %}
-  (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
-  (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
-  m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
-  })(window,document,'script','https://www.google-analytics.com/analytics.js','ga');
-  ga('create', 'UA-82554203-1', 'auto');
-  ga('send', 'pageview');
+	<script async src="https://www.googletagmanager.com/gtag/js?id=G-YP4DF7G826"></script>
+	<script>
+		window.dataLayer = window.dataLayer || [];
+		function gtag(){dataLayer.push(arguments);}
+		gtag('js', new Date());
+	gtag('config', 'G-YP4DF7G826'); // Replace with your actual Measurement ID
+	</script>
 {% endif %}
 
 	var body = document.querySelector("body");
