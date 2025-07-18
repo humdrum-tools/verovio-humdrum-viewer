12844c8c09c71008b8f04aacb00d8d3a08d2a9e8
Craig Stuart Sapp <craigsapp@gmail.com>
2025-07-18 14:30:54 +0200

    Make resource iconts gold colors.

--- _includes/scripts/menu/makeWikipediaIcon.js
+++ _includes/scripts/menu/makeWikipediaIcon.js
@@ -15,8 +15,8 @@ function makeWikipediaIcon(url, title) {
 	output += "style='margin-left:10px !important; margin-right:0px !important; font-size:60%' ";
 	output += `onclick="openOrReplaceWikipedia('${url}')" `;
 
-	output += `<span style="opacity:0.6; margin:0 !important" class="nav-icon fa-stack fa-1x">`;
-	output += `<i style="margin:0 !important; color:white;" class="fas fa-square fa-stack-2x"></i>`;
+	output += `<span style="opacity:0.6; margin:0 !important; color:goldenrod;" class="nav-icon fa-stack fa-1x">`;
+	output += `<i style="margin:0 !important; color:goldenrod" class="fas fa-square fa-stack-2x"></i>`;
 	output += `<i style="margin:0 !important; color:#01313f;" class="fab fa-wikipedia-w fa-stack-1x fa-inverse"></i>`;
 	output += `</span>`;
 
